a3a345c8dbbb12b1bda1c9308287b51d434b3202
letap1121 <sagar2332@icloud.com>
2019-05-26 16:18:30 -0500

    Add files via upload

---
+++ PWA Notes App (Node.js)/README.md
@@ -0,0 +1,2 @@
+# Notes
+<img src="https://sii.im/playground/notes/notes-ss.png" alt="Notes app" width='590px'>
---
+++ PWA Notes App (Node.js)/app.js
@@ -0,0 +1,29 @@
+// app.js
+
+const http = require('http');
+
+// Create an instance of the http server to handle HTTP requests
+let app = http.createServer((req, res) => {
+    // Set a response type of plain text for the response
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send back a response and end the connection
+    res.end('Hello World!\n');
+});
+
+// Start the server on port 3000
+app.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000');
+
+// Registering ServiceWorker
+if ( 'serviceWorker' in navigator ) {
+  navigator.serviceWorker.register( 'sw.js' ).then(function(registration) {
+
+    // Registration was successful
+    console.log( 'ServiceWorker registration successful. Scope: ' + registration.scope )
+  }).catch(function(err) {
+
+    // Registration failed with error
+    console.log( 'ServiceWorker registration failed. Error: ' + err);
+  });
+}
